3942e15727ae03118fa412bc32d0acd73abd61eb
Kerem <keremyilmazerrr@hotmail.com>
2023-07-26 21:43:53 +0300

    Services layer IBookService tanımları

--- WebApi/Extensions/ServicesExtensions.cs
+++ WebApi/Extensions/ServicesExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Repositories.Contracts;
 using Repositories.EFCore;
 
 namespace WebApi.Extensions
@@ -9,7 +10,7 @@ namespace WebApi.Extensions
             IConfiguration configuration) => services.AddDbContext<RepositoryContext>(options =>
                         options.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
 
-
-
+        public static void ConfigureRepositoryManager(this IServiceCollection services) =>
+            services.AddScoped<IRepositoryManager, RepositoryManager>();
     }
 }
