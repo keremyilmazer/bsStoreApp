96b027010a950ce040c3d2912a4428df5cb4e840
Kerem <keremyilmazerrr@hotmail.com>
2023-07-27 20:17:39 +0300

    Exception Handler kullanımı

--- Entities/ErrorModels/ErrorDetails.cs
+++ Entities/ErrorModels/ErrorDetails.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Entities.ErrorModels
 {
-    internal class ErrorDetails
+    public class ErrorDetails
     {
+        public int StatusCode { get; set; }
+        public string? Message { get; set; }
+
     }
 }
