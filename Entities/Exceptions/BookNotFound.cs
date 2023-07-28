1cc55cf3c07ccf22a53402965ea2393a5e8e1bc0
Kerem <keremyilmazerrr@hotmail.com>
2023-07-28 15:46:27 +0300

    Custom NotFound class

---
+++ Entities/Exceptions/BookNotFound.cs
@@ -0,0 +1,10 @@
+﻿namespace Entities.Exceptions
+{
+    public sealed class BookNotFound : NotFound
+    {
+        public BookNotFound(int id) 
+            : base($"The book with id : {id} could not found")
+        {
+        }
+    }
+}
---
+++ Entities/Exceptions/NotFound.cs
@@ -0,0 +1,10 @@
+﻿namespace Entities.Exceptions
+{
+    public abstract class NotFound : Exception
+    {
+        protected NotFound(string message) : base(message)
+        {
+
+        }
+    }
+}
