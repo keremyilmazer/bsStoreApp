e88583e9bed094889267c73e53cbba9d1e9d97bd
Kerem <keremyilmazerrr@hotmail.com>
2023-07-31 22:41:25 +0300

    Konfigrasyona AddXmlDataContractSerializerFormatters eklenmesi

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddControllers(config =>
     config.RespectBrowserAcceptHeader = true;
     config.ReturnHttpNotAcceptable = true;
 })
+    .AddXmlDataContractSerializerFormatters()
     .AddApplicationPart(typeof(Presentation.AssemblyReferance).Assembly);
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
