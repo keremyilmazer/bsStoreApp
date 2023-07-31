a513ba138a0f055410eb256adba70f2857192eb3
Kerem <keremyilmazerrr@hotmail.com>
2023-07-31 18:43:58 +0300

    Mapping Profile tanımı

---
+++ WebApi/Utilities/AutoMapper/MappingProfilecs.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Entities.DataTransferObjects;
+using Entities.Models;
+
+namespace WebApi.Utilities.AutoMapper
+{
+    public class MappingProfilecs : Profile
+    {
+        public MappingProfilecs()
+        {
+            CreateMap<BookDtoForUpdate, Book>();
+        }
+    }
+}
