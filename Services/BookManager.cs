dc4dca1745d2ca0c39edcddd36de1dfc17b6ccdf
Kerem <keremyilmazerrr@hotmail.com>
2023-08-03 16:15:56 +0300

    Service katmanının iyileştirilmesi

--- Services/BookManager.cs
+++ Services/BookManager.cs
@@ -37,15 +37,9 @@ namespace Services
 
         public async Task DeleteOneBookAsync(int id, bool trackChanges)
         {
-            //check entity 
-            var entity = await _manager.Book.GetOneBookByIdAsync(id, trackChanges);
-            
-            if (entity is null)
-                throw new BookNotFoundException(id);
-
+            var entity = await GetOneBookByIdAndCheckExists(id, trackChanges);
             _manager.Book.DeleteOneBook(entity);
             await _manager.SaveAsync();
-
         }
 
         public async Task<IEnumerable<BookDto>> GetAllBooksAsync(bool trackChanges)
@@ -56,24 +50,27 @@ namespace Services
 
         public async Task<BookDto> GetOneBookByIdAsync(int id, bool trackChanges)
         {
-            var book = await _manager.Book.GetOneBookByIdAsync(id, trackChanges);
-            if (book is null)
-                throw new BookNotFoundException(id);
+            var book = await GetOneBookByIdAndCheckExists(id, trackChanges);
             return _mapper.Map<BookDto>(book);
         }
 
         public async Task UpdateOneBookAsync(int id, BookDtoForUpdate bookDto, bool trackChanges)
         {
+            var entity = await GetOneBookByIdAndCheckExists(id, trackChanges);
+            entity = _mapper.Map<Book>(bookDto);
+            _manager.Book.Update(entity);
+            await _manager.SaveAsync();
+        }
+
+        private async Task<Book> GetOneBookByIdAndCheckExists(int id, bool trackChanges)
+        {
             //check entity 
             var entity = await _manager.Book.GetOneBookByIdAsync(id, trackChanges);
+
             if (entity is null)
                 throw new BookNotFoundException(id);
 
-            //Mapping            
-            entity = _mapper.Map<Book>(bookDto);
-
-            _manager.Book.Update(entity);
-            await _manager.SaveAsync();
+            return entity;
         }
     }
 }
